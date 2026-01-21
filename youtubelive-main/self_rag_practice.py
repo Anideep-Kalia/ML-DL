d77eab0215e104d15967b7e55a7c883097a0df46
Anideep-Kalia <anideepkalia@gmail.com>
2026-01-21 17:46:42 +0530

    RAG added

---
+++ youtubelive-main/self_rag_practice.py
@@ -0,0 +1,562 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+LANGCHAIN_API_KEY=os.getenv("LANGCHAIN_API_KEY")
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+
+
+# In[39]:
+
+
+import warnings
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+
+# In[36]:
+
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+
+# In[66]:
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=embeddings,
+)
+
+retriever = vectorstore.as_retriever()
+
+
+# In[67]:
+
+
+from langchain.tools.retriever import create_retriever_tool
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+)
+
+tools = [retriever_tool]
+
+
+# #### Let's look into the retriever grader
+
+# In[68]:
+
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+# Data model
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+# LLM with function call
+structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+
+# In[69]:
+
+
+# Prompt
+system = """You are a grader checking if a document is relevant to a user’s question.The check has to be done very strictly..  
+If the document has words or meanings related to the question, mark it as relevant.  
+Give a simple 'yes' or 'no' answer to show if the document is relevant or not."""
+
+grade_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+    ]
+)
+
+
+# In[70]:
+
+
+retrieval_grader = grade_prompt | structured_llm_grader
+question = "agent memory"
+docs = retriever.get_relevant_documents(question)
+
+
+# In[71]:
+
+
+doc_txt = docs[1].page_content
+
+
+# In[1]:
+
+
+print(retrieval_grader.invoke({"question": question, "document": doc_txt}))
+
+
+# ### let's look into the data generation 
+
+# In[73]:
+
+
+### Generate
+
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+
+prompt = hub.pull("rlm/rag-prompt")
+
+
+# In[74]:
+
+
+prompt.pretty_print()
+
+
+# In[75]:
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+# In[76]:
+
+
+rag_chain = prompt | llm
+
+
+# In[77]:
+
+
+# Run
+generation = rag_chain.invoke({"context": docs, "question": question})
+
+
+# In[78]:
+
+
+generation
+
+
+# ### Hallucination Grader
+
+# In[79]:
+
+
+# Data model
+class GradeHallucinations(BaseModel):
+    """Binary score for hallucination present in generation answer."""
+
+    binary_score: str = Field(
+        description="Answer is grounded in the facts, 'yes' or 'no'"
+    )
+
+
+# LLM with function call
+structured_llm_grader = llm.with_structured_output(GradeHallucinations)
+
+
+# In[80]:
+
+
+# Prompt
+system = """You are a grader checking if an LLM generation is grounded in or supported by a set of retrieved facts.  
+Give a simple 'yes' or 'no' answer. 'Yes' means the generation is grounded in or supported by a set of retrieved the facts."""
+hallucination_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
+    ]
+)
+
+
+# In[81]:
+
+
+hallucinations_grader = hallucination_prompt | structured_llm_grader
+hallucinations_grader.invoke({"documents": docs, "generation": generation})
+
+
+# #### Answer Grader
+
+# In[82]:
+
+
+### Answer Grader
+# Data model
+class GradeAnswer(BaseModel):
+    """Binary score to assess answer addresses question."""
+
+    binary_score: str = Field(
+        description="Answer addresses the question, 'yes' or 'no'"
+    )
+
+
+# LLM with function call
+structured_llm_grader = llm.with_structured_output(GradeAnswer)
+
+# Prompt
+system = """You are a grader assessing whether an answer addresses / resolves a question \n 
+     Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+    ]
+)
+
+answer_grader = answer_prompt | structured_llm_grader
+answer_grader.invoke({"question": question, "generation": generation})
+
+
+# ### Question Re-writer
+
+# In[219]:
+
+
+system = """You are a question re-writer that converts an input question into a better optimized version for vector store retrieval document.  
+You are given both a question and a document.  
+- First, check if the question is relevant to the document by identifying a connection or relevance between them.  
+- If there is a little relevancy, rewrite the question based on the semantic intent of the question and the context of the document.  
+- If no relevance is found, simply return "question not relevant."  
+Your goal is to ensure the rewritten question aligns well with the document for better retrieval."""
+
+re_write_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        (
+            "human","""Here is the initial question: \n\n {question} \n,
+             Here is the document: \n\n {documents} \n ,
+             Formulate an improved question. if possible other return 'question not relevant'."""
+        ),
+    ]
+)
+question_rewriter = re_write_prompt | llm | StrOutputParser()
+
+
+# In[222]:
+
+
+question="who is a current indian prime minister?"
+
+
+# In[223]:
+
+
+question_rewriter.invoke({"question":question,"documents":docs})
+
+
+# ### From here the Langgraph workflow will start
+
+# In[254]:
+
+
+from typing import List
+from typing_extensions import TypedDict
+class AgentState(TypedDict):
+    question: str
+    generation: str
+    documents: List[str]
+    filter_documents: List[str]
+    unfilter_documents: List[str]
+
+
+# In[255]:
+
+
+def retrieve(state:AgentState):
+    print("----RETRIEVE----")
+    question=state['question']
+    documents=retriever.get_relevant_documents(question)
+    return {"documents": documents, "question": question}
+
+
+# In[256]:
+
+
+def grade_documents(state:AgentState):
+    print("----CHECK DOCUMENTS RELEVANCE TO THE QUESTION----")
+    question = state['question']
+    documents = state['documents']
+
+    filtered_docs = []
+    unfiltered_docs = []
+    for doc in documents:
+        score=retrieval_grader.invoke({"question":question, "document":doc})
+        grade=score.binary_score
+
+        if grade=='yes':
+            print("----GRADE: DOCUMENT RELEVANT----")
+            filtered_docs.append(doc)
+        else:
+            print("----GRADE: DOCUMENT NOT RELEVANT----")
+            unfiltered_docs.append(doc)
+    if len(unfiltered_docs)>1:
+        return {"unfilter_documents": unfiltered_docs,"filter_documents":[], "question": question}
+    else:
+        return {"filter_documents": filtered_docs,"unfilter_documents":[],"question": question}
+
+
+
+
+# In[257]:
+
+
+def decide_to_generate(state:AgentState):
+    print("----ACCESS GRADED DOCUMENTS----")
+    state["question"]
+    unfiltered_documents = state["unfilter_documents"]
+    filtered_documents = state["filter_documents"]
+
+
+    if unfiltered_documents:
+        print("----ALL THE DOCUMENTS ARE NOT RELEVANT TO QUESTION, TRANSFORM QUERY----")
+        return "transform_query"
+    if filtered_documents:
+        print("----DECISION: GENERATE----")
+        return "generate"
+
+
+
+# In[258]:
+
+
+def generate(state:AgentState):
+    print("----GENERATE----")
+    question=state["question"]
+    documents=state["documents"]
+
+    generation = rag_chain.invoke({"context": documents,"question":question})
+    return {"documents":documents,"question":question,"generation":generation}
+
+
+# In[282]:
+
+
+from langgraph.graph import END, StateGraph, START
+def transform_query(state:AgentState):
+    question=state["question"]
+    documents=state["documents"]
+
+    print(f"this is my document{documents}")
+    response = question_rewriter.invoke({"question":question,"documents":documents})
+    print(f"----RESPONSE---- {response}")
+    if response == 'question not relevant':
+        print("----QUESTION IS NOT AT ALL RELEVANT----")
+        return {"documents":documents,"question":response,"generation":"question was not at all relevant"}
+    else:   
+        return {"documents":documents,"question":response}
+
+
+# In[283]:
+
+
+def decide_to_generate_after_transformation(state:AgentState):
+    question=state["question"]
+
+    if question=="question not relevant":
+        return "query_not_at_all_relevant"
+    else:
+        return "Retriever"
+
+
+# In[284]:
+
+
+import pprint
+def grade_generation_vs_documents_and_question(state:AgentState):
+    print("---CHECK HELLUCINATIONS---")
+    question= state['question']
+    documents = state['documents']
+    generation = state["generation"]
+
+    score = hallucinations_grader.invoke({"documents":documents,"generation":generation})
+
+    grade = score.binary_score
+
+    #Check hallucinations
+    if grade=='yes':
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+
+        print("---GRADE GENERATION vs QUESTION ---")
+
+        score = answer_grader.invoke({"question":question,"generation":generation})
+
+        grade = score.binary_score
+
+        if grade=='yes':
+            print("---DECISION: GENERATION ADDRESS THE QUESTION ---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---TRANSFORM QUERY")
+            return "not useful"
+    else:
+        pprint("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---TRANSFORM QUERY")
+        "not useful"
+
+
+
+# In[285]:
+
+
+from typing import List
+from typing_extensions import TypedDict
+from langgraph.graph import END, StateGraph, START
+
+workflow = StateGraph(AgentState)
+workflow.add_node("Docs_Vector_Retrieve", retrieve)
+workflow.add_node("Grading_Generated_Documents", grade_documents) 
+workflow.add_node("Content_Generator", generate)
+workflow.add_node("Transform_User_Query", transform_query)
+
+
+# In[286]:
+
+
+workflow.add_edge(START, "Docs_Vector_Retrieve")
+
+
+# In[287]:
+
+
+workflow.add_edge("Docs_Vector_Retrieve","Grading_Generated_Documents")
+
+
+# In[288]:
+
+
+workflow.add_conditional_edges("Grading_Generated_Documents",
+                            decide_to_generate,
+                            {
+                            "generate": "Content_Generator",
+                            "transform_query": "Transform_User_Query"
+                            }
+                            )
+
+
+# In[289]:
+
+
+workflow.add_conditional_edges("Content_Generator",
+                            grade_generation_vs_documents_and_question,
+                            {
+                            "useful": END,
+                            "not useful": "Transform_User_Query",
+                            }
+)
+
+
+# In[290]:
+
+
+workflow.add_conditional_edges("Transform_User_Query",
+                  decide_to_generate_after_transformation,
+                  {"Retriever":"Docs_Vector_Retrieve",
+                   "query_not_at_all_relevant":END})
+
+
+# In[291]:
+
+
+app = workflow.compile()
+
+
+# In[292]:
+
+
+from IPython.display import Image, display # type: ignore
+display(Image(app.get_graph(xray=True).draw_mermaid_png()))
+
+
+# In[297]:
+
+
+inputs = {"question": "Explain how the different types of agent memory work?"}
+
+
+# In[301]:
+
+
+app.invoke(inputs)["generation"]
+
+
+# In[302]:
+
+
+inputs = {"question": "who is a prompt engineering?"}
+
+
+# In[304]:
+
+
+app.invoke(inputs)
+
+
+# In[307]:
+
+
+inputs = {"question": "what is role of data structure while creating agentic pattern?"}
+
+
+# In[308]:
+
+
+app.invoke(inputs)
+
+
+# In[293]:
+
+
+inputs = {"question": "who is a first president of USA?"}
+
+
+# In[295]:
+
+
+app.invoke(inputs)["generation"]
+
+
+# In[ ]:
+
+
+
+
